7fcf80db6006082df3184b069bf606760d087b51
Antea P <antea.primorac.5157@metropolitan.ac.rs>
2024-08-17 20:41:56 +0200

    reformatted Item nicely, added operator overload (for ==), and methods for decrementing quality and checking if item is used up (i.e. has quantity 0)

--- src/Item.cpp
+++ src/Item.cpp
@@ -2,25 +2,35 @@
 
 #include <stdexcept>
 
-Item::Item(int quantity) : quantity(quantity) {}
+Item::Item(int quantity) : quantity(quantity) { }
 
-std::string Item::use(Pokemon& user, Pokemon& target)
-{
-    if (quantity < 1)
-    {
-        throw std::logic_error("There's nothing to decrement!");
-    }
-    --quantity;
-    return useInternal(user, target);
-}
-
-int Item::getQuantity() const
-{
+int Item::getQuantity() const {
     return quantity;
 }
 
-int Item::setQuantity(int newQuantity)
-{
+int Item::setQuantity(int newQuantity) {
     quantity = newQuantity;
     return quantity;
+}
+
+bool Item::operator ==(const std::string &name) const {
+    return this->getName() == name;
+}
+
+void Item::decrementQuantity() {
+    if (quantity > 0) {
+        --quantity;
+    }
+}
+
+bool Item::isUsedUp() const {
+    return quantity == 0;
+}
+
+std::string Item::use(Pokemon &user, Pokemon &target) {
+    if (quantity < 1) {
+        throw std::logic_error("Cannot use depleted item");
+    }
+    decrementQuantity();
+    return useInternal(user, target);
 }
--- src/headers/Item.h
+++ src/headers/Item.h
@@ -1,6 +1,10 @@
 #ifndef ITEM_H
 #define ITEM_H
+
+#include <string>
 #include "Usable.h"
+
+class Pokemon;
 
 class Item : public Usable
 {
@@ -8,7 +12,7 @@ private:
     int quantity;
 
 protected:
-    virtual std::string useInternal(Pokemon& user, Pokemon& target) = 0;
+    virtual std::string useInternal(Pokemon &user, Pokemon &target) = 0;
 
 public:
     explicit Item(int quantity);
@@ -17,8 +21,12 @@ public:
     virtual std::string getName() const override = 0;
     int getQuantity() const;
     int setQuantity(int newQuantity);
+    bool operator ==(const std::string &name) const;
 
-    virtual std::string use(Pokemon& user, Pokemon& target) override;
+    void decrementQuantity();
+    bool isUsedUp() const;
+
+    virtual std::string use(Pokemon &user, Pokemon &target) override;
 };
 
 #endif //ITEM_H
